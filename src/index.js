b5ce8072e4d53421925d3daf665d36cb3782a0ea
Sardor <139249785+sardornext@users.noreply.github.com>
2024-08-07 09:53:18 +0500

    feat: towelSort alg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+  return matrix.map((row, index) => {
+    return index % 2 === 0 ? row : row.reverse();
+  }).reduce((acc, curr) => acc.concat(curr), []);
 }
